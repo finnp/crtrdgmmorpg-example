0001200d906d2396b08e5ededdf0938ad31bffed
Finn <derfinn@gmail.com>
2014-05-24 01:30:36 +0200

    more generic

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 var io = require('socket.io-client');
 
-var socket = io.connect('http://crtrdgmmorpg.herokuapp.com:80');
+var socket = io.connect(location.origin);
 
 // connection test
 socket.on('echo', function (data) {
