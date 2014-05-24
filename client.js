f785d7e31977cc2cd51122159c82759f2261288e
Finn <derfinn@gmail.com>
2014-05-24 12:41:25 +0200

    broadcast changes not positions

--- client.js
+++ client.js
@@ -2,19 +2,9 @@ var io = require('socket.io-client');
 
 var socket = io.connect(location.origin);
 
-// connection test
-socket.on('echo', function (data) {
-  console.log(data);
-});
-
-socket.emit('echo', { the: 'test' });
-
-
 var Game = require('crtrdg-gameloop');
 var Arrows = require('crtrdg-arrows');
-var Mouse = require('crtrdg-mouse');
 var Entity = require('crtrdg-entity');
-var aabb = require('aabb-2d');
 var Vector2 = require('vector2-node'); // http://rahatarmanahmed.github.io/vector2-node/docs/index.html
 
 var inherits = require('inherits');
@@ -27,7 +17,6 @@ var game = new Game({
 });
 
 var arrows = new Arrows();
-var mouse = new Mouse(game);
 
 inherits(Player, Entity);
 
@@ -40,27 +29,24 @@ function Player(id, x, y) {
       y: 10
     };
 
-    this.velocity = 4;
-
-    this.box = aabb(
-      [this.position.x, this.position.y],
-      [this.size.x, this.size.y]
-    );
-
+    this.velocity = 4; // base speed not current
+    this.movement = new Vector2(0, 0); // length of the vector is the speed
 }
 
-// Direction: In Radiants from right counterclockwise
-Player.prototype.controller = function (direction) {
+Player.prototype.controller = function () {
 
   var dx = arrows.isDown('right') - arrows.isDown('left');
   var dy = arrows.isDown('down') - arrows.isDown('up');
   var movement = new Vector2(dx, dy);
-
   if (movement.length() > 0) {
     movement.normalize();
-    this.position.add(movement.scale(this.velocity));
-    socket.emit('position', this.position);
   }
+  movement.scale(this.velocity);
+  if(!movement.equals(this.movement)) {
+    this.movement = movement;
+    socket.emit('change', {pos: this.position, mov: this.movement});
+  }
+
 }
 
 var me = new Player(0, 10, 10);
@@ -68,7 +54,8 @@ me.addTo(game);
 
 var players = [me];
 
-socket.on('position', function (data) {
+socket.on('change', function (data) {
+  console.log('change');
 
   var player = players.filter(function (player) {
     return player.id === data.id;
@@ -78,30 +65,31 @@ socket.on('position', function (data) {
     // Update player
     player.position.x = data.pos.x;
     player.position.y = data.pos.y;
+    player.movement.x = data.mov.x;
+    player.movement.y = data.mov.y;
   } else {
     // Create player
     var newPlayer = new Player(data.id, data.pos.x, data.pos.y);
-    // i should also add them to the game maybe
+    newPlayer.movement.x = data.mov.x;
+    newPlayer.movement.y = data.mov.y;
     players.push(newPlayer);
   }
 
 })
 
-
 me.on('update', function (interval) {
-  this.controller(); // When keys are pressed
+    this.controller(); // When keys are pressed
 });
 
-
-game.on('update', function(interval){});
+game.on('update', function () {
+  players.forEach(function (player) {
+    player.position.add(player.movement);
+  });
+});
 
 game.on('draw', function(context){
   context.fillStyle = '#fff';
   players.forEach(function (player) {
     context.fillRect(player.position.x, player.position.y, player.size.x, player.size.y);
-  })
+  });
 });
-
-game.on('pause', function(){});
-
-game.on('resume', function(){});
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 var http = require('http');
 var send = require('send');
+
 var app = http.createServer(function(req, res){
   console.log(req.url);
   send(req, req.url, {root: __dirname}).pipe(res);
 })
+
 var io = require('socket.io').listen(app);
 
-var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+var port = process.env.PORT || 5000;
 app.listen(port);
 
-// This example server simply takes the positions and broadcasts them to all clients
+// This example server simply takes the player movements and positions and broadcasts them
 
 io.sockets.on('connection', function (socket) {
-
-  socket.on('position', function (position) {
-    socket.broadcast.emit('position', {pos: position, id: socket.id});
-  });
-
-
-
-  // Echo for testing
-  socket.on('echo', function (data) {
-    socket.emit('echo', data);
+  socket.on('change', function (data) {
+    data.id = socket.id;
+    socket.broadcast.emit('change', data);
   });
 });
